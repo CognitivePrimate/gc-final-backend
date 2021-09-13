118d89efcc8c677f51b5de7ffd618b4d4a28dd74
Derek Jackson <d4jackson84@gmail.com>
2021-09-13 19:27:21 -0400

    setup done

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+// imported modules
+import path from "path";
+import express from "express";
+import cors from "cors";
+
+// local imports
+import shoutoutsRoutes from "./routes";
+
+// create instance of express
+const app = express();
+
+// enable ability to parse body of requests
+app.use(express.json());
+
+// enables cors
+app.use(cors());
+
+// declare port server runs on 
+const port = 3000;
+
+// add routes to application
+app.use("/", shoutoutsRoutes);
+
+// start server
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+})
